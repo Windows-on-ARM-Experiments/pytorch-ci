fc2fd91fa82c2948666b1f5c28b9f92afeace941
Radek Bartoň <radek.barton@microsoft.com>
2023-04-01 20:02:23 +0200

    Fix path of cleanup_pytorch.bat

--- .github/scripts/bootstrap_pytorch.bat
+++ .github/scripts/bootstrap_pytorch.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call cleanup_pytorch.bat
+call %GITHUB_WORKSPACE%\pytorch-unit-tests\workflow\.github\scripts\cleanup_pytorch.bat
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
