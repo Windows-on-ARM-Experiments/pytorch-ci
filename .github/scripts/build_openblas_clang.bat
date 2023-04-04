6e56c54cfab4b617b69b86f2a73f41fdd866b3c0
Evgeny Karpov <eukarpov@gmail.com>
2023-04-04 14:52:35 +0200

    Add openblas build VS clang (#1)

--- .github/scripts/build_openblas_clang.bat
+++ .github/scripts/build_openblas_clang.bat
@@ -8,12 +8,11 @@ if "%visualstudio%" == "" (
   goto :eof
 )
 
-
-call "%visualstudio%\VC\Auxiliary\Build\vcvarsall.bat" arm64
-
 mkdir build
 cd build
 
-echo "TODO: Not implemented yet."
+path=%path%;%visualstudio%\VC\Tools\Llvm\ARM64\bin
 
-exit(1)
+cmake ..  -G Ninja -DCMAKE_C_COMPILER=clang -DNOFORTRAN=1 -DDYNAMIC_ARCH=0 -DTARGET=ARMV8 -DARCH=arm64 -DBINARY=64 -DUSE_OPENMP=0 -DCMAKE_SYSTEM_PROCESSOR=ARM64 -DCMAKE_CROSSCOMPILING=1 -DCMAKE_SYSTEM_NAME=Windows
+cmake --build . --config Release
+cmake --install . --prefix ../install
