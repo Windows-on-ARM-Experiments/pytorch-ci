7e7292efd104823027e2cdcded43f95278bce728
Radek Bartoň <radek.barton@microsoft.com>
2023-05-11 09:01:22 +0200

    Disable features that cannot be compiled on Arm64

--- .github/scripts/build_pytorch.bat
+++ .github/scripts/build_pytorch.bat
@@ -10,6 +10,19 @@ call "%visualstudio%\VC\Auxiliary\Build\vcvarsall.bat" arm64
 
 set BLAS=OpenBLAS
 set OpenBLAS_HOME=%GITHUB_WORKSPACE%\pytorch-unit-tests\openblas\install
+set USE_CUDA=OFF
+set USE_DISTRIBUTED=OFF
+set USE_FAKELOWP=OFF
+set USE_FBGEMM=OFF
+set USE_MAGMA=OFF
+set USE_METAL=OFF
+set USE_MKLDNN=OFF
+set USE_NNPACK=OFF
+set USE_PYTORCH_QNNPACK=OFF
+set USE_QNNPACK=OFF
+set USE_ROCM=OFF
+set USE_TENSORPIPE=OFF
+set USE_XNNPACK=OFF
 
 python setup.py install --cmake --home=%GITHUB_WORKSPACE%\pytorch-unit-tests\pytorch\install  
 python setup.py install
