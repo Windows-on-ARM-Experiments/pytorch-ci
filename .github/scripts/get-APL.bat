f457185d1ca27662a01dcd9297cdaf018651ed45
mantaionut <ionut@janeasystems.com>
2023-12-08 11:01:33 +0200

    now really fixed installing APL

--- .github/scripts/get-APL.bat
+++ .github/scripts/get-APL.bat
@@ -5,15 +5,15 @@ set ZIP_FILE=arm-performance-libraries_23.10_Windows.zip
 set DEST_DIR=%JOB_DIR%
 
 :: Check if the ZIP file already exists in the destination directory
-if not exist "%DEST_DIR%%ZIP_FILE%" (
+::if not exist "%DEST_DIR%%ZIP_FILE%" (
     echo File not found. Downloading...
     curl -L -o "%ZIP_FILE%" "%FILE_URL%"
     echo Unzipping the file...
 	tar -xf "%ZIP_FILE%" -C %DEST_DIR%
 	del %ZIP_FILE%
 	
-) else (
-    echo File already exists. Skipping download and unzip.
-)
+::) else (
+::    echo File already exists. Skipping download and unzip.
+::)
 set PATH=%PATH%;%DEST_DIR%\%FILE_NAME%\armpl_23.10
 echo Done.
