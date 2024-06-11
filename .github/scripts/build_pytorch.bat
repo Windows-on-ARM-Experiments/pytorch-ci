7554d7d7b51ace506d92025770d33f02d6c9ec50
Ozan Aydin <148207261+ozanMSFT@users.noreply.github.com>
2024-06-11 02:32:26 +0200

    - build test

--- .github/scripts/build_pytorch.bat
+++ .github/scripts/build_pytorch.bat
@@ -1,58 +1,47 @@
 @echo on
+:: start sccache server and reset sccache stats
+sccache --zero-stats
 
+:: environment variables
+:: TODO: OpenBLAS implementation will be applied
+set BLAS=APL
+set REL_WITH_DEB_INFO=1
+set CMAKE_BUILD_TYPE=RelWithDebInfo
+set USE_LAPACK=1
+set USE_NUMPY=OFF
+set CMAKE_C_COMPILER_LAUNCHER=sccache
+set CMAKE_CXX_COMPILER_LAUNCHER=sccache
+
+:: change to source directory
+cd pytorch
+
+:: submodule sync
+git submodule sync
+git submodule update --init --recursive
+ 
+:: create virtual environment
+python -m venv venv
+echo * > venv\.gitignore
+call .\venv\Scripts\activate
+
+:: python install dependencies
+pip install -r requirements.txt
+pip install pytest==8.1.1 pytest-xdist==3.5.0 pytest-shard pytest-rerunfailures==13.0 pytest-flakefinder pytest-pytorch expecttest hypothesis xdoctest
+ 
+:: activate visual studio
 set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 set visualstudio=
-for /f "delims=" %%v in ('"%vswhere%" -latest -property installationPath') do set "visualstudio=%%v"
+for /f "delims=" %%v in ('"%vswhere%" -products Microsoft.VisualStudio.Product.BuildTools -latest -property installationPath') do set "visualstudio=%%v"
 if "%visualstudio%" == "" (
   echo Visual Studio not found!
   goto :eof
 )
 
-if "%ENABLE_APL%" == "1" (
-  set PATH=%PATH%;%ARMPL_DIR%\bin
-)
-
-call "%visualstudio%\VC\Auxiliary\Build\vcvarsall.bat" arm64
-
-cd %JOB_DIR%\pytorch
-
-set REL_WITH_DEB_INFO=1
-set CMAKE_BUILD_TYPE=RelWithDebInfo
-
-echo %ENABLE_OPENBLAS%
-
-if "%ENABLE_OPENBLAS%" == "1" (
-  set BLAS=OpenBLAS
-  set OpenBLAS_HOME=%JOB_DIR%\openblas\install
-)
-
-:: start sccache server and reset sccache stats
-set PATH=%PATH%;C:\sccache
-sccache --zero-stats
-
-set USE_LAPACK=1
-set USE_CUDA=OFF
-set USE_DISTRIBUTED=OFF
-set USE_FAKELOWP=OFF
-set USE_FBGEMM=OFF
-set USE_MAGMA=OFF
-set USE_METAL=OFF
-set USE_MKLDNN=OFF
-set USE_NNPACK=OFF
-set USE_PYTORCH_QNNPACK=OFF
-set USE_QNNPACK=OFF
-set USE_ROCM=OFF
-set USE_TENSORPIPE=OFF
-set USE_XNNPACK=OFF
-set BUILD_PYTHON=False
-set CMAKE_GENERATOR=Ninja
-set CMAKE_C_COMPILER_LAUNCHER=sccache
-set CMAKE_CXX_COMPILER_LAUNCHER=sccache
-set CMAKE_CUDA_COMPILER_LAUNCHER=sccache
-python setup.py install --cmake --home=%JOB_DIR%\pytorch\install
+:: TODO: vcvars_ver is optional but currently it is being tested with 14.36 and 14.40
+cmd /k "%visualstudio%\VC\Auxiliary\Build\vcvarsall.bat" arm64 -vcvars_ver=14.40
 
 :: show sccache stats
 sccache --show-stats
 
-python setup.py install
+python setup.py bdist_wheel
 
