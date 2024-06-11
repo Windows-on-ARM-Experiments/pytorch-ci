0e410272eb3aff20ec5dd4271af719016aab9202
Ozan Aydin <148207261+ozanMSFT@users.noreply.github.com>
2024-06-11 02:41:20 +0200

    - echo off test

--- .github/scripts/build_pytorch.bat
+++ .github/scripts/build_pytorch.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 :: start sccache server and reset sccache stats
 sccache --zero-stats
 
--- .github/scripts/wipe_openblas_sources.bat
+++ .github/scripts/wipe_openblas_sources.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 if exist [%OPENBLAS_SOURCES_DIR%] (
   rmdir /s /q %OPENBLAS_SOURCES_DIR%
