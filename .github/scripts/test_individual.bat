5e6cd0aded1fe78d688bd1343fdaf12e34c755da
alinpahontu2912 <alin_pahontu@yahoo.com>
2024-06-28 13:51:09 +0200

    test

--- .github/scripts/test_individual.bat
+++ .github/scripts/test_individual.bat
@@ -1,28 +1,16 @@
 :: change to source directory
 cd %PYTORCH_SOURCES_DIR%
 
-@REM python -c "import torch" > nul 2>&1
-@REM if errorlevel 1 (
-@REM     echo Torch module not found in local env
-@REM     exit /b 1
-@REM ) else (
-@REM     echo Torch module found locally.
-@REM )
-
 :: create virtual environment
-python -m venv venv
-echo * > venv\.gitignore
 call .\venv\Scripts\activate
-echo Debug: Before python path
 where python
-echo Debug: After python path
 
 python -c "import torch" > nul 2>&1
 if errorlevel 1 (
-    echo  Torch module not found in venv
+    echo Torch module not found in local env
     exit /b 1
 ) else (
-    echo Torch module found in venv.
+    echo Torch module found locally.
 )
 
 :: change to test directory
