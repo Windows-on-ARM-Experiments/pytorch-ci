4d171dc513097e5c9054fff0910c0ca20721c7e5
alinpahontu2912 <alin_pahontu@yahoo.com>
2024-06-28 12:41:29 +0200

    test - add torch to venv

--- .github/scripts/test_individual.bat
+++ .github/scripts/test_individual.bat
@@ -7,11 +7,18 @@ echo * > venv\.gitignore
 call .\venv\Scripts\activate
 where python
 
+:: link the local PyTorch build to the virtual environment
+pip install -e .
+
 :: change to test directory
 cd test
 
 if "%3"=="" (
     python run_test.py -i %2 --verbose --save-xml --keep-going --runs %1
 ) else (
-    python run_test.py -i %2 --verbose --save-xml --keep-going --runs %1 --filter "%3" 
+    python run_test.py -i %2 --verbose --save-xml --keep-going --runs %1 --filter "%3"
 )
+
+:: deactivate and delete the virtual environment
+deactivate
+rd /s /q venv
