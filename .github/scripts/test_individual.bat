1cbff41b61ee3e9e3d3751f23a488784d6fe45d4
alinpahontu2912 <alin_pahontu@yahoo.com>
2024-07-01 16:03:56 +0200

    discard run_test.py

--- .github/scripts/test_individual.bat
+++ .github/scripts/test_individual.bat
@@ -2,22 +2,16 @@
 cd %PYTORCH_SOURCES_DIR%
 
 :: create virtual environment
+python -m venv venv
+echo * > venv\.gitignore
 call .\venv\Scripts\activate
 where python
-
-python -c "import torch" > nul 2>&1
-if errorlevel 1 (
-    echo Torch module not found in local env
-    exit /b 1
-) else (
-    echo Torch module found locally.
-)
 
 :: change to test directory
 cd test
 
 if "%3"=="" (
-    python run_test.py -i %2 --verbose --save-xml --keep-going --runs %1
+    python %2 --verbose -vvvv -rfEsxXP -p no:xdist --use-pytest --save-xml -k "deadlock"
 ) else (
     python run_test.py -i %2 --verbose --save-xml --keep-going --runs %1 --filter "%3" 
 )
