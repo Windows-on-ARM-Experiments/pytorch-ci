434183b4b3f5880928aa319a927adf1e09fd2d4c
alinpahontu2912 <alin_pahontu@yahoo.com>
2024-07-22 15:26:17 +0200

    add installation of wheels

--- .github/scripts/bootstrap_wheels.bat
+++ .github/scripts/bootstrap_wheels.bat
@@ -1,16 +1,14 @@
 @echo off
 echo Downloading numpy and Scipy wheels for Windows ARM64...
 
-::set DEPENDENCIES_DIR=C:\Users\spahontu\env\dependencies
-:: set DOWNLOADS_DIR=C:\Users\spahontu\env\downloads
-
 :: Pre check for downloads and dependencies folders
 if not exist "%DOWNLOADS_DIR%" mkdir "%DOWNLOADS_DIR%"
 if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
-REM Detect Python version
+
+:: Detect Python version
 for /f "tokens=2 delims= " %%i in ('python -V 2^>^&1') do set PYTHON_VERSION=%%i
 
-REM Extract the major and minor version numbers
+:: Extract the major and minor version numbers
 for /f "tokens=1-2 delims=." %%i in ("%PYTHON_VERSION%") do (
     set PYTHON_MAJOR=%%i
     set PYTHON_MINOR=%%j
@@ -18,7 +16,7 @@ for /f "tokens=1-2 delims=." %%i in ("%PYTHON_VERSION%") do (
 
 echo Detected Python version: %PYTHON_MAJOR%.%PYTHON_MINOR%
 
-REM Determine the download URL and file names based on the Python version
+:: Determine the download URL and file names based on the Python version
 if "%PYTHON_MAJOR%.%PYTHON_MINOR%"=="3.11" (
     set "DOWNLOAD_URL=https://github.com/cgohlke/win_arm64-wheels/releases/download/v2023.12.6/2023.12.6-experimental-cp311-win_arm64.whl.zip"
     set "ZIP_FILE=%DOWNLOADS_DIR%\2023.12.6-experimental-cp311-win_arm64.whl.zip"
@@ -32,12 +30,12 @@ if "%PYTHON_MAJOR%.%PYTHON_MINOR%"=="3.11" (
     exit /b 1
 )
 
-REM Check if the extraction directory already exists
+:: Check if the extraction directory already exists
 if not exist "%DEPENDENCIES_DIR%\%EXTRACT_DIR%" (
     echo Downloading ZIP file from %DOWNLOAD_URL%...
     curl -L -o "%ZIP_FILE%" %DOWNLOAD_URL%
 
-    REM Check if download was successful
+    :: Check if download was successful
     if %errorlevel% neq 0 (
         echo Failed to download the ZIP file.
         exit /b 1
@@ -57,17 +55,14 @@ if not exist "%DEPENDENCIES_DIR%\%EXTRACT_DIR%" (
 
 cd "%DEPENDENCIES_DIR%\%EXTRACT_DIR%"
 dir
-@REM pip install numpy
-@REM pip install scipy
+
+if "%PYTHON_MAJOR%.%PYTHON_MINOR%"=="3.11" (
+    pip install numpy-1.26.2-cp311-cp311-win_arm64.whl
+    pip install SciPy-1.11.4-cp311-cp311-win_arm64.whl
+) else (
+    pip install numpy-2.0.0-cp312-cp312-win_arm64.whl
+    pip install scipy-1.13.1-cp312-cp312-win_arm64.whl
+)
 
 echo Dependency installation is finished.
-
-if exist "%DOWNLOADS_DIR%" (
-    echo Deleting downloads directory...
-    rd /s /q "%DOWNLOADS_DIR%"
-)
-if exist "%DEPENDENCIES_DIR%" (
-    echo Deleting dependencies directory...
-    rd /s /q "%DEPENDENCIES_DIR%"
-)
 
