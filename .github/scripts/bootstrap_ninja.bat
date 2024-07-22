cb827bd5cc8f895f71bec8c304e019d178cebb74
alinpahontu2912 <alin_pahontu@yahoo.com>
2024-07-22 12:23:29 +0200

    test  path

--- .github/scripts/bootstrap_ninja.bat
+++ .github/scripts/bootstrap_ninja.bat
@@ -1,34 +1,35 @@
-@echo off
-echo Dependency ninja installation is started.
+echo "this is a test"
+@REM @echo off
+@REM echo Dependency ninja installation is started.
 
-:: set DEPENDENCIES_DIR=C:\Users\spahontu\env\dependencies
-:: set DOWNLOADS_DIR=C:\Users\spahontu\env\downloads
+@REM :: set DEPENDENCIES_DIR=C:\Users\spahontu\env\dependencies
+@REM :: set DOWNLOADS_DIR=C:\Users\spahontu\env\downloads
 
-:: Pre check for downloads and dependencies folders
-if not exist "%DOWNLOADS_DIR%" mkdir "%DOWNLOADS_DIR%"
-if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
+@REM :: Pre check for downloads and dependencies folders
+@REM if not exist "%DOWNLOADS_DIR%" mkdir "%DOWNLOADS_DIR%"
+@REM if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
 
-set DOWNLOAD_URL="https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-win.zip"
-set TARGET_DIR=ninja
-set TARGET_FILE=ninja.exe
+@REM set DOWNLOAD_URL="https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-win.zip"
+@REM set TARGET_DIR=ninja
+@REM set TARGET_FILE=ninja.exe
 
-:: Check if the file already exists in the destination directory
-:: TODO: smarter check mechanism can be used (e.g. call with error level)
-if not exist "%DEPENDENCIES_DIR%\%TARGET_DIR%\%TARGET_FILE%" (
-    echo Downloading ninja.zip...
-    curl -L -o "%DOWNLOADS_DIR%\ninja.zip" %DOWNLOAD_URL%
+@REM :: Check if the file already exists in the destination directory
+@REM :: TODO: smarter check mechanism can be used (e.g. call with error level)
+@REM if not exist "%DEPENDENCIES_DIR%\%TARGET_DIR%\%TARGET_FILE%" (
+@REM     echo Downloading ninja.zip...
+@REM     curl -L -o "%DOWNLOADS_DIR%\ninja.zip" %DOWNLOAD_URL%
 
-    echo Extracting ninja.zip...
-    mkdir "%DEPENDENCIES_DIR%\%TARGET_DIR%"
-    tar xf "%DOWNLOADS_DIR%\ninja.zip" -C "%DEPENDENCIES_DIR%\%TARGET_DIR%"
-    echo Successfully installed: ninja
-) else (
-    echo Skipped download and install: ninja
-)
+@REM     echo Extracting ninja.zip...
+@REM     mkdir "%DEPENDENCIES_DIR%\%TARGET_DIR%"
+@REM     tar xf "%DOWNLOADS_DIR%\ninja.zip" -C "%DEPENDENCIES_DIR%\%TARGET_DIR%"
+@REM     echo Successfully installed: ninja
+@REM ) else (
+@REM     echo Skipped download and install: ninja
+@REM )
 
-echo "%DEPENDENCIES_DIR%\%TARGET_DIR%" >> %GITHUB_PATH%
+@REM echo "%DEPENDENCIES_DIR%\%TARGET_DIR%" >> %GITHUB_PATH%
 
-echo Dependency ninja installation is finished
+@REM echo Dependency ninja installation is finished
 
-ninja --version
+@REM ninja --version
 
