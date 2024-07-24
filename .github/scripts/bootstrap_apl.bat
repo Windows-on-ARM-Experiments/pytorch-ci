f50aff1a3f9184a913cf55b9c47d53dcdf480f10
iremyux <iremyuksel@microsoft.com>
2024-07-24 12:46:14 +0200

    test

--- .github/scripts/bootstrap_apl.bat
+++ .github/scripts/bootstrap_apl.bat
@@ -1,9 +1,9 @@
 :: For now, APL is considered as already installed (script is not in use)
 
 @echo off
+setlocal enabledelayedexpansion
 
 echo Dependency APL installation is started.
-echo Test enable-bootstrap-apl
 :: Pre check for downloads and dependencies folders
 if not exist "%DOWNLOADS_DIR%" mkdir "%DOWNLOADS_DIR%"
 if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
@@ -11,6 +11,21 @@ if not exist "%DEPENDENCIES_DIR%" mkdir "%DEPENDENCIES_DIR%"
 set DOWNLOAD_URL="https://developer.arm.com/-/media/Files/downloads/hpc/arm-performance-libraries/24-04/windows/arm-performance-libraries_24.04_Windows.msi?rev=2b30cf856f7d4e6b968ce7f3698c1088&revision=2b30cf85-6f7d-4e6b-968c-e7f3698c1088"
 set TARGET_DIR=apl\armpl_24.04\bin
 set TARGET_FILE=armpl-info.exe
+
+:: Check if the Windows Installer Service is running
+sc query msiserver | find "RUNNING" >nul
+if !errorlevel!==0 (
+    echo Windows Installer Service is already running.
+) else (
+    echo Attempting to start the Windows Installer Service...
+    net start msiserver
+    if !errorlevel!==0 (
+        echo Windows Installer Service started successfully.
+    ) else (
+        echo Failed to start the Windows Installer Service. Please check your permissions and try again.
+        exit /b 1
+    )
+)
 
 :: Check if the file already exists in the destination directory
 :: TODO: smarter check mechanism can be used (e.g. call with error level)
